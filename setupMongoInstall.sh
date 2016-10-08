9ba4e20d79a18910ada49954d595aa7a395e162b
Vagrant Default User <vagrant@debian-jessie.vagrantup.com>
2016-10-08 12:42:21 +0000

    Added mongo install files.

---
+++ setupMongoInstall.sh
@@ -0,0 +1,2 @@
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+sudo echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.2 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
