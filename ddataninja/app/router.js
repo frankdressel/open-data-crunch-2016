01a1c7529b0d5026365ed4507932b1a666acf2fc
frankdressel <frankdressel@users.noreply.github.com>
2016-10-14 23:40:48 +0000

    Playing around with ember.

--- ddataninja/app/router.js
+++ ddataninja/app/router.js
@@ -7,6 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('intro-page');
+  this.route('about');
+  this.route('contact');
 });
 
 export default Router;
---
+++ ddataninja/tests/acceptance/intro-page-test.js
@@ -0,0 +1,43 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'ddataninja/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | intro page');
+
+test('visiting /intro-page', function(assert) {
+  visit('/intro-page');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/intro-page');
+  });
+});
+
+test('should redirect to intro page', function (assert) {
+  visit('/');
+  andThen(function() {
+      assert.equal(currentURL(), '/intro-page', 'should redirect automatically');
+    });
+});
+
+test('should link to information about the company.', function (assert) {
+  visit('/');
+  click('a:contains("About")');
+  andThen(function () {
+      assert.equal(currentURL(), '/about', 'should navigate to about');
+    });
+});
+
+test('should link to contact information', function (assert) {
+  visit('/');
+  click('a:contains("Contact")');
+  andThen(function () {
+      assert.equal(currentURL(), '/contact', 'should navigate to contact');
+    });
+});
+
+test('should show qr and score image.', function (assert) {
+  visit('/intro-page');
+  andThen(function () {
+      assert.equal(find('.qr-code').length, 1, 'should show qr code');
+      assert.equal(find('.score-image').length, 1, 'should show score image');
+    });
+});
